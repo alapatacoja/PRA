e5eb64f3e82ebff5514785ca4b6b690b372746d4
alapatacoja <medaigualsihackean@gmail.com>
2024-09-17 11:22:17 +0200

    ghfhg

---
+++ tarea1.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+class Ordenacion {
+
+   private:
+   //atributos
+    vector<int> vec;
+    int size;
+
+    public:
+    //constructor y destructor
+    
+
+};
