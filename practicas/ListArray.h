e0d9f6806934f9fa21e1be81405970c0ac6e32cb
alex <alex>
2024-09-26 17:35:09 +0200

    dhask

---
+++ practicas/ListArray.h
@@ -0,0 +1,13 @@
+#include <ostream>
+#include "List.h"
+
+template <typename T> 
+class ListArray : public List<T> {
+
+    private:
+        // miembros privados
+
+    public:
+        // miembros públicos, incluidos los heredados de List<T>
+    
+};
