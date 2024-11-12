424faac9be066bda59e885a63115767e856ecaaf
alapatacoja <medaigualsihackean@gmail.com>
2024-11-12 20:55:26 +0100

    final

--- practicas/practica1/ListLinked.h
+++ practicas/practica1/ListLinked.h
@@ -47,4 +47,80 @@ class ListLinked : public List<T> {
             }
             return out;
         }
+
+        void insert(int pos, T e){
+		if (pos < 0 || pos > n) {
+			throw std::out_of_range("fuera de rango");
+		}
+
+		resize(max + 1);
+		
+		for (int i = n; i > pos; --i) {
+			arr[i] = arr[i - 1];
+		}
+
+		arr[pos] = e;
+		++n;
+	}
+
+	void append(T e){
+		insert(n, e);
+	}
+
+	void prepend(T e){
+		insert(0, e);
+	}
+
+	T remove(int pos){
+		if (pos < 0 || pos >= n) {
+			throw std::out_of_range("Position out of range");
+		}
+
+		T e = arr[pos];
+		for (int i = pos; i < n - 1; ++i) {
+			arr[i] = arr[i + 1];
+		}
+
+		--n;
+
+		resize(max - 1);
+
+		return e;
+	}
+
+	T get(int pos){
+		return T[pos];
+	}
+
+	int search(T e){
+		for (int i = 0; i < n; ++i) {
+			if (arr[i] == e) {
+				return i;
+			}
+		}
+		return -1;
+	}
+
+	bool empty(){
+		if(sizeof(arr)==0)
+			return true;
+		else 
+			return false;
+	}
+
+	int size(){
+		return sizeof(arr);
+	}
+
+	void resize(int new_size){
+		T* new_arr = new T[new_size];
+
+		for (int i = 0; i < n; i++) {
+			new_arr[i] = arr[i];
+		}
+
+		delete[] arr;
+		arr = new_arr;
+		max = new_size;
+	}
 };
