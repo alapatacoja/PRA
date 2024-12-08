74ab74bea0d2e81c58a732116af87673c2158b9c
Tu Nombre <Tu Nombre>
2024-12-08 20:11:35 +0100

    alkd

--- practicas/practica3/HashTable.h
+++ practicas/practica3/HashTable.h
@@ -9,93 +9,102 @@
 #include "../practica1/ListLinked.h"  
 template <typename V>
 class HashTable: public Dict<V> {
+private:
+    int n;
+    int max;
+    ListLinked<TableEntry<V>>* table;
 
-    private:
-        int n;
-        int max;
-        ListLinked<TableEntry<V>> *table;
+    int h(const std::string& key) {
+        int sum = 0;
+        for (char c : key) {
+            sum += static_cast<int>(c);
+        }
+        return (sum % max + max) % max;
+    }
 
-        int h(std::string key){
-            int sum = 0;
-            for(int i = 0; i<key.size(); i++){
-                sum += int(key.at(i));
+public:
+    HashTable(int size) {
+        if (size <= 0) throw std::invalid_argument("Tamaño inválido");
+        table = new ListLinked<TableEntry<V>>[size];
+        max = size;
+        n = 0;
+    }
+
+    ~HashTable() {
+        delete[] table;
+    }
+
+    int capacity() {
+        return max;
+    }
+
+    friend std::ostream& operator<<(std::ostream& out, const HashTable<V>& th) {
+        for (int i = 0; i < th.max; i++) {
+            out << i << ": " << th.table[i] << std::endl;
+        }
+        return out;
+    }
+
+    V operator[](std::string key) {
+    int temp = h(key);
+    for (int i = 0; i < table[temp].size(); i++) {
+        if (table[temp][i].key == key) {
+            return table[temp][i].value;
+        }
+    }
+    throw std::runtime_error("Key does not exist");
+}
+
+
+    void insert(const std::string key, const V val) {
+    int temp = h(key);
+    // Busca si ya existe una entrada con la misma clave
+    for (int i = 0; i < table[temp].size(); i++) {
+        if (table[temp][i].key == key) {
+            throw std::runtime_error("Key already exists");
+        }
+    }
+    // Inserta la nueva clave-valor
+    TableEntry<V> entry(key, val);
+    table[temp].insert(0, entry);
+    n++;
+}
+
+
+    V search(const std::string& key) {
+        int temp = h(key);
+        if (table[temp].empty()) {
+            throw std::runtime_error("No se encontró la clave");
+        }
+
+        for (int i = 0; i < table[temp].size(); i++) {
+            if (table[temp][i].key == key) {
+                return table[temp][i].value;
             }
-            return sum%max;
+        }
+        throw std::runtime_error("No se encontró la clave");
+    }
+
+    V remove(const std::string& key) {
+        int temp = h(key);
+        if (table[temp].empty()) {
+            throw std::runtime_error("No se encontró la clave");
         }
 
-    public:
-
-        HashTable(int size){
-            table = new ListLinked<TableEntry<V>>[size];
-            max = size;
-            n = 0;
-        }
-
-
-        ~HashTable(){
-            delete[] table;
-        }
-
-        int capacity(){
-            return max;
-        }   
-
-        friend std::ostream& operator<<(std::ostream &out, const HashTable<V> &th){
-            for (int i = 0; i < th.max; i++){
-                out << i << ": " << th.table[i] << std::endl;
+        for (int i = 0; i < table[temp].size(); i++) {
+            if (table[temp][i].key == key) {
+                V val = table[temp][i].value;
+                table[temp].remove(i);
+                n--;
+                return val;
             }
-            return out;
         }
+        throw std::runtime_error("No se encontró la clave");
+    }
 
-	
-        V operator[](std::string key){
-            int temp = h(key);
-            for(int i = 0; i < table[temp].size(); i++){
-                if(table[temp][i].key == key){
-                    return table[temp][i].value;
-                }
-            }
-            throw std::runtime_error("no existe la llave");
-        }
-
-       void insert(const std::string key, const V val){
-            int temp = h(key);
-            try {
-                search(key);
-            } catch (const std::runtime_error&) {
-                TableEntry<V> entry(key, val);
-                table[temp].insert(0, entry);
-                n++;
-            }
-       }
-
-        V search(std::string key){
-            int temp = h(key);
-            for(int i = 0; i < table[temp].size(); i++){
-                if(table[temp][i].key == key){
-                    return table[temp][i].value;
-                }
-            }
-            throw std::runtime_error("No se encontro la llave");
-        }
-
-        V remove(std::string key){
-            int temp = h(key);
-            for(int i = 0; i < table[temp].size(); i++){
-                if(table[temp][i].key == key){
-                    V temp = table[temp][i].value;
-                    table[temp].remove(i);
-                    n--;
-                    return temp;
-                }
-            }
-            throw std::runtime_error("No se encontro la llave");
-        }
-
-        int entries(){
-            return n;
-        }
-        
+    int entries() {
+        return n;
+    }
 };
 
-#endif
+#endif // HASHTABLE_H
